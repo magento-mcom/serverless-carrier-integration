682c19638c56bab76e4f5f8e1a53adaa2edbeee3
Jaume Capdevila Pérez <contact@jaumecapdevila.net>
2020-04-15 16:10:02 +0200

    Remove not used dependency

--- src/Application/TrackingDetailsGenerator.php
+++ src/Application/TrackingDetailsGenerator.php
@@ -7,16 +7,27 @@ namespace Magento\CarrierMock\Application;
 final class TrackingDetailsGenerator
 {
     private const CARRIER = 'carrier_mock';
+    private const LINK = 'https://www.sci.test/?trackingNumber=%s';
 
     public function __invoke(array $packages): array
     {
         $updated = [];
 
         foreach ($packages as $package) {
+            $trackingNumber = $this->randomTrackingNumber();
+
             $package['carrier'] = self::CARRIER;
+            $package['tracking_number'] = $trackingNumber;
+            $package['tracking_link'] = sprintf(self::LINK, $trackingNumber);
+
             $updated[] = $package;
         }
 
         return $updated;
+    }
+
+    private function randomTrackingNumber(): int
+    {
+        return random_int(0, 10 ** 6);
     }
 }
